50d8a1b24b13c17228b78a0e202d623c97d8ec48
SpLiYvIiI <atukadagio123@gmail.com>
2021-10-02 22:12:11 +0400

    3.2

--- part3/part3_2/racxa.sh
+++ part3/part3_2/racxa.sh
@@ -1,1 +1,26 @@
-docker login
+#!/bin/bash
+echo "enter your docker username : "
+
+read username 
+
+echo "enter your docker password : "
+
+read -s password
+
+docker login -u=$username -p=$password
+
+echo "enter github reposiotry url : "
+
+read url
+
+git clone $url
+
+cd $( echo $url | cut -d/ -f5-6)
+
+echo "enter image tag : " 
+
+read imagetag
+
+docker build . -t $username/$imagetag
+
+docker push $username/$imagetag
